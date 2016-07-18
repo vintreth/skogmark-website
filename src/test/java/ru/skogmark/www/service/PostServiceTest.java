4251776bb545b448ef2c7b515b556e785e99a1bc
kbogdanov <kbogdanov@brand-in-trend.ru>
2016-07-18 18:45:36 +0300

    mockito

--- src/test/java/ru/skogmark/www/service/PostServiceTest.java
+++ src/test/java/ru/skogmark/www/service/PostServiceTest.java
@@ -1,6 +1,10 @@
 package ru.skogmark.www.service;
 
 import junit.framework.TestCase;
+import org.mockito.Mockito;
+import ru.skogmark.www.domain.Post;
+
+import java.sql.Date;
 
 /**
  * @author svip
@@ -9,6 +13,15 @@ import junit.framework.TestCase;
 public class PostServiceTest extends TestCase {
 
     public void testGetCreatedAtString() throws Exception {
+        PostService postService = new PostService();
 
+        Post post1 = Mockito.mock(Post.class);
+        Mockito.when(post1.getCreatedAt()).thenReturn(Date.valueOf("2016-01-01"));
+
+        Post post2 = Mockito.mock(Post.class);
+        Mockito.when(post2.getCreatedAt()).thenReturn(Date.valueOf("2016-07-25"));
+
+        assertEquals("1 January 2016", postService.getCreatedAtString(post1));
+        assertEquals("25 July 2016", postService.getCreatedAtString(post2));
     }
 }
