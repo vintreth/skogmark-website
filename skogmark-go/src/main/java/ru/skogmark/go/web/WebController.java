a39aa0dc171a272cbdb2cf92819242d3ac85d78b
svip <bogdanow90@gmail.com>
2016-12-18 13:21:45 +0300

    blogger dependencies

--- skogmark-go/src/main/java/ru/skogmark/go/web/WebController.java
+++ skogmark-go/src/main/java/ru/skogmark/go/web/WebController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class WebController {
     private static final String INDEX_JSP = "index";
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index() {
         return INDEX_JSP;
     }
