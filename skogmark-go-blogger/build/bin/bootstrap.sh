daeb7fae20a2c1dd3e9a556a9f79f0c9200b4500
svip <bogdanow90@gmail.com>
2017-01-12 16:05:21 +0300

    config location fix

--- skogmark-go-blogger/build/bin/bootstrap.sh
+++ skogmark-go-blogger/build/bin/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONFIG_LOCATION=${DIR}/conf
+CONFIG_LOCATION=${DIR}/../conf
 LOG_CONF=file:${CONFIG_LOCATION}/logback.xml
 JAR_NAME=app.jar
 
