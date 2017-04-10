37b4a498aff3264c2479a04896d909c88b497dc1
svip <bogdanow90@gmail.com>
2017-04-10 18:45:48 +0300

    fixed bootstrap problems

--- skogmark-go-gen/src/main/java/ru/skogmark/go/gen/App.java
+++ skogmark-go-gen/src/main/java/ru/skogmark/go/gen/App.java
@@ -1,11 +1,17 @@
 package ru.skogmark.go.gen;
 
+import org.apache.catalina.authenticator.jaspic.AuthConfigFactoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.security.auth.message.config.AuthConfigFactory;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        if (AuthConfigFactory.getFactory() == null) {
+            AuthConfigFactory.setFactory(new AuthConfigFactoryImpl());
+        }
         SpringApplication.run(App.class, args);
     }
 }
