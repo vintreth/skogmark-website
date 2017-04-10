45bd07d9070fa50d32624778f424f15744db58f7
svip <bogdanow90@gmail.com>
2017-04-10 15:21:54 +0300

    building blogger distribution

--- skogmark-go-blogger/build/bin/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONFIG_LOCATION=${DIR}/../conf
-LOG_CONF=file:${CONFIG_LOCATION}/log4j.xml
-JAR_NAME=app.jar
-
-java -jar -Dlog4j.configuration=${LOG_CONF} -Dapp.configLocation=${CONFIG_LOCATION} ${DIR}/${JAR_NAME} <&- &
