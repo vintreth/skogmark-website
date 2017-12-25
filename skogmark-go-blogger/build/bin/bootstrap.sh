6500df5c25e2afd19289571d681f0c0c672bf6a6
ksbogdanov <bogdanow90@gmail.com>
2017-12-25 13:30:36 +0300

    logging

--- skogmark-go-blogger/build/bin/bootstrap.sh
+++ skogmark-go-blogger/build/bin/bootstrap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/java -Dapp.configLocation=/opt/blogger/conf -DLogback.configurationFile=/opt/blogger/conf/logback.xml -jar /opt/blogger/lib/skogmark-go-blogger-1.3-SNAPSHOT.jar
+/usr/bin/java -Dapp.configLocation=/opt/blogger/conf -jar /opt/blogger/lib/skogmark-go-blogger-1.3-SNAPSHOT.jar
--- skogmark-go-gen/build/bin/bootstrap.sh
+++ skogmark-go-gen/build/bin/bootstrap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/java -DLogback.configurationFile=/opt/generator/conf/logback.xml -jar /opt/generator/lib/skogmark-go-gen-1.3-SNAPSHOT.jar
+/usr/bin/java -jar /opt/generator/lib/skogmark-go-gen-1.3-SNAPSHOT.jar
