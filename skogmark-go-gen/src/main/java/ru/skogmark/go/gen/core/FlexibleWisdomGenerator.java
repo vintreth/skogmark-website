40e1565afacd33ebb903987d983dc470573f774d
ksbogdanov <ksbogdanov@yamoney.ru>
2017-12-27 16:26:00 +0300

    fix

--- skogmark-go-gen/src/main/java/ru/skogmark/go/gen/core/FlexibleWisdomGenerator.java
+++ skogmark-go-gen/src/main/java/ru/skogmark/go/gen/core/FlexibleWisdomGenerator.java
@@ -4,22 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import ru.skogmark.go.api.Wisdom;
 import ru.skogmark.go.gen.core.pipeline.WisdomPayload;
 import ru.skogmark.go.gen.core.pipeline.Pipeline;
-import ru.skogmark.go.gen.core.pipeline.WisdomPipelineFactory;
+import ru.skogmark.go.gen.core.pipeline.WisdomPipelineBuilder;
 
 /**
  * New flexible implementation of {@link WisdomGenerator}
  */
 public class FlexibleWisdomGenerator implements WisdomGenerator {
-    private final WisdomPipelineFactory pipelineFactory;
+    private final WisdomPipelineBuilder pipelineBuilder;
 
     @Autowired
-    public FlexibleWisdomGenerator(WisdomPipelineFactory pipelineFactory) {
-        this.pipelineFactory = pipelineFactory;
+    public FlexibleWisdomGenerator(WisdomPipelineBuilder pipelineBuilder) {
+        this.pipelineBuilder = pipelineBuilder;
     }
 
     @Override
     public Wisdom generateOne() {
-        Pipeline<WisdomPayload> pipeline = pipelineFactory.getPipeline();
+        Pipeline<WisdomPayload> pipeline = pipelineBuilder.build();
         WisdomPayload payload = new WisdomPayload();
         pipeline.process(payload);
         return null;
