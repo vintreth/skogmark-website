bbce2f24eb32315a1dadbe161d47b9ab676aac3f
vintreth <bogdanow90@gmail.com>
2018-12-06 00:11:43 +0300

    content-generator

---
+++ skogmark-content-generator/src/main/java/ru/skogmark/contentgenerator/ContentGeneratorApplication.java
@@ -0,0 +1,11 @@
+package ru.skogmark.contentgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentGeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentGeneratorApplication.class, args);
+    }
+}
